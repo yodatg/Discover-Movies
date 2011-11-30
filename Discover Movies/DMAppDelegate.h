bb903e9bb6a24ab34a798439f4b0a06e672aacfa
Thomas Grant <tomgrant00@gmail.com>
2011-11-30 16:27:13 +0000

    Initial Commit

---
+++ Discover Movies/DMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DMAppDelegate.h
+//  Discover Movies
+//
+//  Created by Thomas Grant on 30/11/2011.
+//  Copyright (c) 2011 Reading University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Discover Movies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Discover Movies
+//
+//  Created by Thomas Grant on 30/11/2011.
+//  Copyright (c) 2011 Reading University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DMAppDelegate class]));
+    }
+}
